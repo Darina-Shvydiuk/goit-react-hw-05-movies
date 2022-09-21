480dffb4817ada3c0d82c9b24f649b66041fb724
Darina-Shvydiuk <104676845+Darina-Shvydiuk@users.noreply.github.com>
2022-09-21 14:40:01 +0000

    Deploying to gh-pages from @ Darina-Shvydiuk/goit-react-hw-05-movies@646487909de8fca9a6492a5985f0580a9fb9b57c 🚀

---
+++ static/js/273.1280b279.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[273],{2273:function(D,e,B){B.r(e),B.d(e,{default:function(){return t}});var r=B(184),t=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("img",{src:"https://thumbs.dreamstime.com/z/%D0%BE%D1%88%D0%B8%D0%B1%D0%BA%D0%B0-404-%D0%B2%D1%8B%D0%B7%D1%8B%D0%B2%D0%B0%D0%B5%D1%82-%D0%BD%D0%B5-%D0%BD%D0%B0%D0%B9%D0%B4%D0%B5%D0%BD%D0%BE-26920014.jpg",alt:"404 Error"}),(0,r.jsx)("p",{children:"Sorry page not found"})]})}}}]);
+//# sourceMappingURL=273.1280b279.chunk.js.map
